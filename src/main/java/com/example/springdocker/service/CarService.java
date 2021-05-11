cf9d3f368c69cff72fe945428f7803476d755960
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2021-05-11 13:32:00 +0200

    edited testrow

--- src/main/java/com/example/springdocker/service/CarService.java
+++ src/main/java/com/example/springdocker/service/CarService.java
@@ -30,7 +30,7 @@ public class CarService {
 
     public List<String> getRunnableCars() {
         // hämtar alla Cars som vi kan köra
-        // liten testrad
+        //editerade testraden
         List<Car> runnableCars = repository.findCarsByCanIDriveIt(true);
 
         // returnerar endast cars namnen i en lista
